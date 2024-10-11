9d42ad523ff94931630b2f5c82e36c62f15fcdeb
Eugene <webeugene@proton.me>
2024-10-11 17:12:41 +0300

    quicksort test

--- sort/quicksort/quicksort.go
+++ sort/quicksort/quicksort.go
@@ -1,10 +1,15 @@
+// quicksort.go
+// description: Implementation of in-place quicksort algorithm.
+// details:
+// A simple in-place quicksort algorithm implementation. [Wikipedia](https://en.wikipedia.org/wiki/Quicksort)
+// see quick_sort_test.go for a test implementation.
+
 package quicksort
 
 import (
 	"math/rand"
 )
 
-// quickSort implement the QuickSort algorithm (https://en.wikipedia.org/wiki/Quicksort), which is a divide-and conquer sorting technique. It recursively partitions the input array based on a pivot element, and sorts the subarrays to the left (elements < the pivot) and right (elements > the pivot).
 func quickSort(arr []int) []int {
         // Base case: if the array has 1 or fewer elements, it is already sorted.
         if len(arr) <= 1 {
@@ -14,7 +19,7 @@ func quickSort(arr []int) []int {
         // Randomly select a pivot element from the array.
         pivot := arr[rand.Intn(len(arr))]
 
-        // Partition the array into three subarrays: 
+        // Partition the array into three subarrays:
         // - `left`: elements less than the pivot,
         // - `equal`: elements equal to the pivot (to handle duplicates),
         // - `right`: elements greater than the pivot.
