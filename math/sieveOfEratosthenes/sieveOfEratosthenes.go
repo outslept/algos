6c89d8355b68943de91cd14a3ed339e7780516c6
Eugene <webeugene@proton.me>
2024-10-11 22:04:26 +0300

    sieveOfEratosthenes impl in golang

---
+++ math/sieveOfEratosthenes/sieveOfEratosthenes.go
@@ -0,0 +1,16 @@
+package sieveoferatosthenes
+
+func sieveOfEratosthenes(n int) []bool {
+	isPrime := make([]bool, n+1)
+	for i := 2; i <= n; i++ {
+		isPrime[i] = true
+	}
+	for i := 2; i*i <= n; i++ {
+		if isPrime[i] {
+			for j := i * i; j <= n; j += i {
+				isPrime[j] = false
+			}
+		}
+	}
+	return isPrime
+}
