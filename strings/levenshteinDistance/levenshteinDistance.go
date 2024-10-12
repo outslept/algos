5db2cf04ae1c7086edbf2620631482df58d37cc3
Eugene <webeugene@proton.me>
2024-10-12 23:17:48 +0300

    levenshteinDistance impl in golang

---
+++ strings/levenshteinDistance/levenshteinDistance.go
@@ -0,0 +1,36 @@
+package levenstein
+
+func levenshteinDistance(s1, s2 string) int {
+	m := len(s1)
+	n := len(s2)
+	d := make([][]int, m+1)
+	for i := range d {
+		d[i] = make([]int, n+1)
+	}
+
+	for i := 0; i <= m; i++ {
+		d[i][0] = i
+	}
+	for j := 0; j <= n; j++ {
+		d[0][j] = j
+	}
+
+	for j := 1; j <= n; j++ {
+		for i := 1; i <= m; i++ {
+			if s1[i-1] == s2[j-1] {
+				d[i][j] = d[i-1][j-1]
+			} else {
+				d[i][j] = min(d[i-1][j]+1, min(d[i][j-1]+1, d[i-1][j-1]+1))
+			}
+		}
+	}
+
+	return d[m][n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
