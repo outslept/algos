93aad2f6c7bb468d843bee2d97206417dc654c67
Eugene <webeugene@proton.me>
2024-10-13 21:55:41 +0300

    HSLToRGB commented

--- colors/hsl_to_rgb.go
+++ colors/hsl_to_rgb.go
@@ -1,7 +1,16 @@
+// hsl_to_rgb.go
+// description: Implementation of HSL to RGB color conversion.
+// details:
+// This file contains a function to convert colors from the HSL (Hue, Saturation, Lightness)
+// color model to to the RGB (Red, Green, Blue) color model.
+// See https://en.wikipedia.org/wiki/HSL_and_HSV for more information on the HSL color model.
+
 package hsl_to_rgb
 
 import "math"
 
+// HSLToRGB converts a color from HSL to RGB color space.
+// It takes three uint8 values representing HSL (0-360, 0-100, 0-100) and returns three uint8 values for RGB (0-255).
 func HSLToRGB(h, s, l uint8) (r, g, b uint8) {
 	h /= 360
 	s /= 100
@@ -30,21 +39,28 @@ func HSLToRGB(h, s, l uint8) (r, g, b uint8) {
 	return uint8(r1 * 255), uint8(g1 * 255), uint8(b1 * 255)
 }
 
+// hueToRGB is a helper function for HSLToRGB.
+// It converts the hue to RGB value.
 func hueToRGB(p, q, t float64) float64 {
 	if t < 0 {
 		t += 1
 	}
+
 	if t > 1 {
 		t -= 1
 	}
+
 	if t < 1/6 {
 		return p + (q-p)*6*t
 	}
+
 	if t < 1/2 {
 		return q
 	}
+
 	if t < 2/3 {
 		return p + (q-p)*(2/3-t)*6
 	}
+
 	return p
 }
